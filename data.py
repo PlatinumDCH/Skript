06f27ccab14c9ebb9914001a37f7959c43513b42
dima cheban <dimacheban23@gmail.com>
2024-11-02 23:33:41 +0200

    move file to py dir

--- data.py
+++
@@ -1,4 +0,0 @@
-from short_url import shorten_url_with_tiny
-
-url = 'https://medium.com/@noransaber685/getting-started-with-sqlalchemy-core-for-beginners-part2-5d8f9becad3f'
-short_url = shorten_url_with_tiny(url)
