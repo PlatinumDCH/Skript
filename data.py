6fa67f57d924e73ecb2e2d0d078e9f77a7a55ce9
dima cheban <dimacheban23@gmail.com>
2024-11-02 22:55:13 +0200

    added data file for import

---
+++ data.py
@@ -0,0 +1,4 @@
+from short_url import shorten_url_with_tiny
+
+url = 'https://medium.com/@noransaber685/getting-started-with-sqlalchemy-core-for-beginners-part2-5d8f9becad3f'
+short_url = shorten_url_with_tiny(url)
